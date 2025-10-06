7d725587d94777724aaef57b846e5d831db24f34
Alex Johnson <112223622+AeJohnson13@users.noreply.github.com>
2025-10-05 23:43:26 -0800

    did the thing

--- contigsum.hpp
+++ contigsum.hpp
@@ -1,53 +1,61 @@
 // contigsum.hpp
 // Alex E. Johnson
 
-// For CS 411 Fall 2025 
+// For CS 411 Fall 2025
 // Used in assignment 3, Exercise A
 // There is no associated source file
 #ifndef FILE_CONTIGSUM_HPP_INCLUDED
 #define FILE_CONTIGSUM_HPP_INCLUDED
 
-
-
 #include <iterator>
 #include <algorithm>
 using std::max;
+#include <vector>
+using std::vector;
+#include <iostream>
+using std::cout;
+using std::endl;
 
 
-template<typename RAIter>
-int contigSum(RAIter first, RAIter last);
-
-
-template<typename RAIter>
-int sumIter(RAIter first, RAIter last){
-    int sum = 0;
-    for(auto iter = first; iter != last; ++iter){
-        sum += *iter;
+template <typename RAIter>
+vector<int> contigSumRecur( RAIter first, RAIter last){
+    auto length = last - first;
+    // Base Case
+    
+    if(length == 1){
+        return {*first, *first, *first, *first};
+   
     }
-    return sum;
+    // Recursive Case
+    else{
+        auto middle = first + length/2;
+        auto left = contigSumRecur(first, middle);
+        auto right = contigSumRecur(middle, last); 
+
+
+        //maximum subarray 
+        int a = max({left[0],right[0],(left[2]+right[1])});
+        //maximum left aligned array
+        int b = max({left[1],left[3]+right[1]});
+        //maximum right aligned array
+        int c = max({right[2],right[3]+left[2]});
+        //total sum
+        int d = left[3] + right[3];
+        
+        return {a, b, c, d};
+    }
 }
 
-
-template<typename RAIter>
-int contigSum(RAIter first, RAIter last){
-
-//Base Case
-if(first == last)
+template <typename RAIter>
+int contigSum(RAIter first, RAIter last)
 {
-    return *first; 
-}
-
-//Recursive Case
-RAIter middle = first-last/2;
-int b = contigSum(first, middle);
-int c = contigSum(middle, last);
-int d = sumIter(first, last);
-
-return std::max({b,c,(b+c)});
+    if(first == last)
+        return 0;
+    
+    auto result = contigSumRecur(first, last);
+    return max(0, result[0]);
 }
 
 
 
-
-
-#endif //#ifndef FILE_CONTIGSUM_HPP_INCLUDED
+#endif // #ifndef FILE_CONTIGSUM_HPP_INCLUDED
